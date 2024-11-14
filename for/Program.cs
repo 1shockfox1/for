14b9969d605bb9a1b77edd83ee5c03c9f2dc7cf9
user <user@DESKTOP-DV6KRKO>
2024-11-14 16:54:19 +0200

    Добавьте файлы проекта.

---
+++ for/Program.cs
@@ -0,0 +1,31 @@
+﻿try
+{
+    Console.WriteLine("введите число D:");
+    int D = int.Parse(Console.ReadLine());
+    if (D <= 0)
+    {
+        Console.WriteLine("D должно быть больше 0!");
+        return;
+    }
+    double A1 = 1;
+    double A2 = 2;
+    double AK = A2;
+    double A3 = (A1 + A2) / 2;
+
+
+
+    for (int k = 2; Math.Abs(AK - A2) < D; k++)
+    {
+
+        AK = (A1 + A2) / 2;
+        A1 = A2;
+        A2 = AK;
+        Console.WriteLine($"k={k} Ak-1={A2} Ak= {A3} ");
+        break;
+    }
+}
+
+catch
+{
+    Console.WriteLine("ошибочка 404....");
+}
